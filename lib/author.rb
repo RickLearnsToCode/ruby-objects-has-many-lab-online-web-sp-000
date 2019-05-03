b89c9c4b3df751225afec747ac9e736968abcbb4
Rick Rosetti <rick.rosetti@gmail.com>
2019-05-03 18:29:01 +0000

    Automatically backed up by Learn

---
+++ lib/author.rb
@@ -0,0 +1,33 @@
+class Author
+  attr_accessor :name
+  # attr_reader :posts
+  @posts = []
+  @@all_posts = []
+
+
+  def initialize(name)
+    @name = name
+  end
+
+  def add_post(post)
+    post.author = self
+    @posts << post
+    @@all_posts << post unless @@all_posts.include?(post)
+  end
+
+  def add_post_by_title(title)
+    post = Post.new(title)
+    post.author = self
+    @posts << post
+    @@all_posts << post unless @@all_posts.include?(post)
+  end
+
+  def self.post_count
+    @@all_posts.length
+  end
+
+
+
+
+
+end
